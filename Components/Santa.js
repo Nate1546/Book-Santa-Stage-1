c0520d1c14dff4b00014e5fe309ec6ccb10422b2
Nate1546 <58136525+Nate1546@users.noreply.github.com>
2020-06-12 04:09:33 -0700

    Add files via upload

---
+++ Components/Santa.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import LottieView from 'lottie-react-native';
+
+export default class SantaAnimation extends React.Component {
+  render() {
+    return (
+      <LottieView
+      source={require('../assets/Santa.json')}
+      style={{width:"60%"}}
+      autoPlay loop />
+    )
+  }
+}
